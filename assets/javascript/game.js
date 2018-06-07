bd75d4fdaac55b98cba71d668bf15322c44cd9d0
Jonathon Wells <jonathonwells10@gmail.com>
2018-06-06 22:05:34 -0500

    adding js updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// Institute Variables
+
+var targetNumber = "";
+var wins = 0;
+var losses = 0;
+var counter = 0;
+var images = ["./assets/images/Blue-Crystal.jpeg", "./assets/images/Green-Crystal.jpeg", "./assets/images/Pink-Crystal.jpeg", "./assets/images/Purple-Crystal.jpeg"];
+
+// Functions
